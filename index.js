e14a69bf70a167125b5401680f80d01b4af93b06
Pawel Bialek <pawel.bialek@e-file.pl>
2021-06-14 00:08:48 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const app = express();
+const app = express()
 
 app.use(
     cookieSession({
